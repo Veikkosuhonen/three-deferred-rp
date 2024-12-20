1ce398796f067a8f680e94233d982e4b6fb899d7
Veikkosuhonen <vesuvesuh@gmail.com>
2024-12-20 11:21:42 +0200

    Solid start => in the trash bin it goes

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,10 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
+import { Component } from "solid-js";
+import Container from "./game/Container";
 
-export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+const App: Component = () => {
+    return (
+        <Container />
+    );
 }
+
+export default App;
--- src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-import { clientOnly } from "@solidjs/start";
-
-const GameContainer = clientOnly(() => import('../game/Container'));
-
-export default function Home() {
-  return <GameContainer />;
-}
